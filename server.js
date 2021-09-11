849ab31854ac31c8d7ea7b98e2cd3299f97e2a34
deweesa <asadeweese@gmail.com>
2021-09-10 19:42:43 -0700

    URLs now have http(s) stripped before being passed to `dns.lookup()`
    So now we are able to take any url and determine if it is a valid url,
    either by looking at it and determining it has no protocol information
    or asking `dns.lookup()` what it thinks.
    
    What i'm working on right now is to ask the database if the full url
    is in any of the records. But having issues with how to get the data out
    of `Model.find()`.

--- server.js
+++ server.js
@@ -2,6 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+const { doesNotMatch } = require('assert');
+const { callbackify } = require('util');
+const { Schema } = mongoose;
+
+const urlSchema = new Schema({
+  original_url: {type: String, required: true},
+  short_url: {type: Number, required: true}
+})
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
+.catch((err) => {
+  console.log(`cant connect to mongodb. error: $(err)`);
+})
+
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -19,6 +40,60 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', (req, res) => {
+  const options = {
+    family: 6,
+    hints: dns.ADDRCONFIG | dns.V4MAPPED
+  }
+
+  let UrlPair = mongoose.model('UrlPair', urlSchema);
+
+  
+
+  const error = {error: 'invalid url'};
+  var message;
+
+  console.log(req.body.url);
+
+  const fullUrl = req.body.url
+  if (fullUrl.search(/(http)s*:\/\//) === -1) {
+    res.json(error);
+    return;
+  }
+
+  const abbreviated = fullUrl.replace(/(http)s*:\/\//, '');
+
+  console.log(`full url: ${fullUrl}\nabrv url: ${abbreviated}`)
+  dns.lookup(abbreviated, options, (err, address) => {
+    if (err) message = error;
+    message = {orginal_url: fullUrl, short_url: 0}
+
+    //need to check to see if it's in the database, 
+    //  -> NO: generate short_url id
+    //      |-> check to see if id is already assigned to url
+    //      |       -> YES: put in db
+    //      |       ->  NO: create new short url try again
+    //  -> YES: return url and short url from db
+
+    //check to see if url is in db
+    var queryResult = UrlPair.find({original_url: fullUrl}).exec();
+
+    console.log(`result: ${queryResult}`);
+    var isEmpty = queryResult.then((doc) => {
+      console.log(doc);
+      console.log('in the queryresult then');
+    })
+
+    console.log(isEmpty);
+
+    //console.log(queryResult);
+    //if(!queryResult) console.log('no documents');
+    res.json(message);
+  })
+});
+
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
